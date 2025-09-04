1d1715d8075e725b39467762d3112425dd4febc6
icelain <icelain@ice.computer>
2025-09-04 11:07:20 +0530

    clippy fixes

--- src/local/mod.rs
+++ src/local/mod.rs
@@ -8,8 +8,8 @@ mod reverse;
 pub async fn start_local(
     config: LocalConfig,
 ) -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
-    return match config.tunnel_type {
+    match config.tunnel_type {
         TunnelType::Forward => forward::forward_local(config).await,
         TunnelType::Reverse => reverse::reverse_local(config).await,
-    };
+    }
 }
